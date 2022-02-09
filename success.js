338261c94b5570e409cbedb1d6909e2f47c5b6c7
reasonableguy <michael@michaelrobertson.ca>
2022-02-09 09:43:21 -0800

    Update success.js

--- success.js
+++ success.js
@@ -50,24 +50,7 @@
 			</ul>
 		</div>
 	</div>
-<style>
-.visible{
-	display:block;
-}
-</style>
- <div id="thanks" class="visible" style="display:none;z-index:100;position:fixed;top:0;left:0;width:80%;margin-left:10%;height:80%;margin-top:10%;background-color:red;">
-<h3>cheers boyo!</h3>
 
-		</div>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script>
-$(document).ready(function(){
-  $("body").click(function(){
-    $("#thanks").removeClass("visible");
-  });
-});
-</script>
 </div>
 <div w3-include-html="footer.html"></div>
 
