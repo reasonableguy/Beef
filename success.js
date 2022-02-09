028332166d4df69c5fe647c9c53df6fc37da1581
reasonableguy <michael@michaelrobertson.ca>
2022-02-09 10:34:58 -0800

    Update success.js

--- success.js
+++ success.js
@@ -22,15 +22,24 @@
     top: 20px;
     font-size: 32px;  content: "\00d7"; /* This will render the 'X' */
 }
+
+#thanks {
+	padding:15% 5%;display:none;z-index:100;position:fixed;top:0;left:0;width:80%;margin-left:10%;height:80%;margin-top:10%;background-color:white;
+}
+#thanks p {
+	line-height: 2rem;    max-width: 50ch;    margin: 0 auto;    font-size: 1.3em;
+}
+.cat {
+	height: 200px;    background-size: contain;    background-image: url("https://d33wubrfki0l68.cloudfront.net/803fb523-df28-43d6-a7fa-82bf575ae282/h-xuan-hoang-d-ng-WL_kPEF3eLA-unsplash.jpg");    background-repeat: no-repeat;    margin: 10% auto 0;    background-position: center;
+}
 </style>
 	
 <div class="page background" style="top:0;left:0;width:100%;height:100%;">
 
-
- <div id="thanks" class="visible" style="padding:15% 5%;display:none;z-index:100;position:fixed;top:0;left:0;width:80%;margin-left:10%;height:80%;margin-top:10%;background-color:white;">
+ <div id="thanks" class="visible" >
 <h3>Thanks for adding your recipe</h3>
-<p style="line-height: 2rem;    max-width: 50ch;    margin: 0 auto;    font-size: 1.3em;">Your recipe will soon be added for others to enjoy. The work is being performed by our highly skilled army of farm cats. It takes about a day but may be longer if it's a nap-in-the-sun sort of day.</p>
-     <div style="height: 200px;    background-size: contain;    background-image: url(https://d33wubrfki0l68.cloudfront.net/803fb523-df28-43d6-a7fa-82bf575ae282/h-xuan-hoang-d-ng-WL_kPEF3eLA-unsplash.jpg);    background-repeat: no-repeat;    margin: 10% auto 0;    background-position: center;"		</div>
+<p>Your recipe will soon be added for others to enjoy. The work is being performed by our highly skilled army of farm cats. It takes about a day but may be longer if it's a nap-in-the-sun sort of day.</p>
+     <div class="cat">		</div>
 
 </div>
 
