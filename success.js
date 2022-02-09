8ca5883ce4e88c6bd371b01afac83fa3d3a25c69
reasonableguy <michael@michaelrobertson.ca>
2022-02-08 18:24:30 -0800

    Update success.js

--- success.js
+++ success.js
@@ -9,12 +9,50 @@
 
 	
 
-   
+ <div style="position:absolute;top:0;left:0;width:80%;margin-left:10%;height:80%;margin-top:10%;background-color:red;>
+<h3>cheers boyo!</h3>
+
+		</div>
 	
 <div class="page background" style="top:0;left:0;width:100%;height:100%;">
 	<h1>
-thanks	</h1>
-
+	Recipes by Beef Cuts
+	</h1>
+	<div class="grid home">
+		<div class="grid-item">
+			<h3>Cheaper Cuts</h3>
+			<ul>
+				<li><a href="cheaper-cuts/ground-beef">Ground</a></li>
+				<li><a href="cheaper-cuts/stew-beef">Stew</a></li>
+				<li><a href="cheaper-cuts/ribs">Maui Ribs</a></li>
+			</ul>
+		</div>
+		<div class="grid-item">
+			<h3>Steaks</h3>
+			<ul>
+				<li><a href="steak/tenderloin">Tenderloin</a></li>
+				<li><a href="steak/ny-strip-sirloin">NY Strip Sirloin</a></li>
+				<li><a href="steak/chuck-steak">Chuck</a></li>
+				<li><a href="steak/rib-steak">Rib</a></li>
+			</ul>
+		</div>
+				<div class="grid-item">
+			<h3>Roasts</h3>
+			<ul>
+				<li><a href="roasts/cross-rib">Cross Rib</a></li>
+				<li><a href="roasts/inside-round">Inside Round</a></li>
+			</ul>
+		</div>
+		<div class="grid-item">
+			<h3>The Rest</h3>
+			<ul>
+				<li><a href="the-rest/beef-heart">Heart</a></li>
+				<li><a href="the-rest/beef-bones">Bones</a></li>
+				<li><a href="the-rest/beef-livers">Livers</a></li>
+				<li><a href="the-rest/beef-kidneys">Kidneys</a></li>
+			</ul>
+		</div>
+	</div>
 </div>
 <div w3-include-html="footer.html"></div>
 
