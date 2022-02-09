5a0d9e53257c922a1cd81da76fe0ef67a51ea610
reasonableguy <michael@michaelrobertson.ca>
2022-02-08 18:19:28 -0800

    Update success.js

--- success.js
+++ success.js
@@ -1,1 +1,26 @@
-hiuo
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="stylesheet.css"> 
+</head>
+	<body>
+	<div w3-include-html="/header.html"></div>
+
+	
+
+   
+	
+<div class="page background" style="top:0;left:0;width:100%;height:100%;">
+	<h1>
+thanks	</h1>
+
+</div>
+<div w3-include-html="footer.html"></div>
+
+
+   
+
+	
+</body>
+</html>
